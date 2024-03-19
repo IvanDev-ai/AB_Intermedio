cadcac03291607fb682843191ac86a40c53e588a
IvanDev <123570888+IvanDev-ai@users.noreply.github.com>
2024-03-19 14:26:34 +0100

    Update

--- proxy.js
+++ proxy.js
@@ -6,13 +6,23 @@ app.use(cors());
 
 app.get('/api', async (req, res) => {
     try {
-        const response = await fetch('https://es.wikipedia.org/w/api.php?action=query&list=random&rnlimit=1&format=json');
-        const data = await response.json();
-        const randomPage = data.query.random[0];
-        const title = randomPage.title;
-        const description = randomPage.snippet;
+        const request = require('request');
+        const word = "";
+        request.get({
+            url: 'https://api.api-ninjas.com/v1/randomword',
+            headers: {'X-Api-Key': 'YEBrft4Rq/1zfW2aa2m6Ug==JP3LfbWMmVVAUe7j'},
+        }, function(error, response, body) {
+            if(error) return console.error('Request failed:', error);
+            else if(response.statusCode != 200) return console.error('Error:', response.statusCode, body.toString('utf8'));
+            else word = word = JSON.parse(body)[0];
+        });
 
-        res.json({ title, description });
+        const response2 = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+        const definition = await response2.json();
+        const firstDefinition = definition[0].meanings[0].definitions[0].definition;
+        console.log(firstDefinition); // Imprime la primera definición        
+
+        res.json({word, firstDefinition});
     } catch (error) {
         console.error('Error al obtener la palabra aleatoria:', error);
         res.status(500).json({ error: 'Error al obtener la palabra aleatoria' });
